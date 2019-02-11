fe54cc9fcce033f75dd23aca0049d052d76695c9
Michael Nguyen <michaelnguyen974@gmail.com>
2019-02-11 15:21:52 +0000

    feat_test: depositing money adds to balance in statement class

--- spec/BankStatementSpec.js
+++ spec/BankStatementSpec.js
@@ -7,4 +7,9 @@ describe("BankStatement", () => {
   it("checks if Bankstatement has balance of 0", () => {
     expect(statement.balance).toEqual(0)
   })
+
+  it("checks if we can add an amount", () => {
+    statement.deposit(20)
+    expect(statement.balance).toEqual(20)
+  })
 })
